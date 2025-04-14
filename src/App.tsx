7e1bd1a94fc1185edce81bc50933339685fefc59
Takeno90 <takehiro.moro@gmail.com>
2025-04-14 21:22:15 +0900

    20250413_カレンダー部作成

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,12 @@ function App() {
           <Route path="/" element={<AppLayout />}>
             <Route
               index
-              element={<Home monthlyTransactions={monthlyTransactions} />}
+              element={
+                <Home
+                  monthlyTransactions={monthlyTransactions}
+                  setCurrentMonth={setCurrentMonth}
+                />
+              }
             />
             <Route path="/report" element={<Report />} />
             <Route path="*" element={<NoMatch />} />
